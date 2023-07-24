9eaee3cabfd40faa4c352d320b3a0c7a1b77c519
Bo Cao <bo_cao@yahoo.com>
2023-07-24 11:48:35 +0200

    formatted source code

--- main.py
+++ main.py
@@ -1,33 +1,33 @@
 from src.data_cleaning import cleaning
-from src.data_cleaning import remove_outliners
-from src.model import train_test,linear_regression,decision_tree_regression,random_forest_regression,XGBoost_regression
+from src.data_cleaning import remove_outliers
+from src.model import train_test, linear_regression, decision_tree_regression, random_forest_regression, XGBoost_regression
 import pandas as pd
 
 # import and clean the data
-raw_data= pd.read_csv(".\data\data_cleaned.csv",index_col=[0])
+raw_data = pd.read_csv(".\data\data_cleaned.csv", index_col=[0])
 df = cleaning(raw_data)
-outliners_to_remove=["number of bedrooms","livable area","living area",
-                           "total property area","garden area","total land area","terrace area"]
-df = remove_outliners(df,outliners_to_remove,3)
+outliners_to_remove = ["number of bedrooms", "livable area", "living area",
+                       "total property area", "garden area", "total land area", "terrace area"]
+df = remove_outliers(df, outliners_to_remove, 3)
 
-#remove extreme prices (price > price.mean() + 4 * std)
-df = remove_outliners(df,["price"],4)
+# remove extreme prices (price > price.mean() + 4 * std)
+df = remove_outliers(df, ["price"], 4)
 
-#split the data and create X_train, X_test, y_train, y_test with normalized array
-X_train, X_test, y_train, y_test=train_test(df,seed = 21,test_size=0.2)
+# split the data and create X_train, X_test, y_train, y_test with normalized array
+X_train, X_test, y_train, y_test = train_test(df, seed=21, test_size=0.2)
 
-#Use Linear Regression
+# Use Linear Regression
 print("---Using Linear Regression---")
 linear_regression(X_train, X_test, y_train, y_test)
 
-#Use Decision Tree regressor
+# Use Decision Tree regressor
 print("\n---Using Decision Tree---")
 decision_tree_regression(X_train, X_test, y_train, y_test)
 
-#Use Decision Tree regressor
+# Use Decision Tree regressor
 print("\n---Using Random Forest---")
 random_forest_regression(X_train, X_test, y_train, y_test)
 
-#Use Decision Tree regressor
+# Use Decision Tree regressor
 print("\n---Using RGBoost---")
 XGBoost_regression(X_train, X_test, y_train, y_test)
--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 from sklearn import preprocessing
+
+
 def _convert(n):
     """get the first two digits of postalcode"""
     return int(n/100)
@@ -9,43 +11,45 @@ def _convert(n):
 def cleaning(df):
     """Cleaning the dataframe by:
     1.replace True with 1, and False with 0
-    2.replace categorical data with number
+    2.replace categorical data with numbera
     3.scale the data"""
 
-    #drop Nan rows
-    df.drop(df["kitchen"].isna().index)
+    # drop Nan rows
+    df.drop(df["kitchen"].isna().index, inplace=True)
 
-    #remove the properties without coordinates
+    # remove the properties without coordinates
     lon_la = (df["latitude"].isna()) | (df["longitude"].isna())
-    df.drop(df.loc[lon_la].index,inplace=True)
+    df.drop(df.loc[lon_la].index, inplace=True)
 
-    #remove castles
-    castle = df[df["subtype of property"]=="CASTLE"].index
-    df.drop(castle,inplace=True)
+    # remove castles
+    castle = df[df["subtype of property"] == "CASTLE"].index
+    df.drop(castle, inplace=True)
 
-    #remove fully equipped kitchen column
-    df.drop(columns=["fully equipped kitchen"],inplace=True)
+    # remove fully equipped kitchen column
+    df.drop(columns=["fully equipped kitchen"], inplace=True)
 
     # add a column for the first two digits of the postal code
-    df["digit"]=df["postalCode"].agg(_convert)
+    df["digit"] = df["postalCode"].agg(_convert)
 
-    #get dummies for catagorical data:
-    get_dummies = ["Province","type of property","kitchen","subtype of property",
-                   "state of the building","garden","terrace","digit"]
-    df =pd.get_dummies(data=df, columns=get_dummies)
+    # get dummies for catagorical data:
+    get_dummies = ["Province", "type of property", "kitchen", "subtype of property",
+                   "state of the building", "garden", "terrace", "digit"]
+    df = pd.get_dummies(data=df, columns=get_dummies)
 
-    #normalize these columns
-    columns_to_normalize = ["latitude","longitude","living area","livable area",
-                        "garden area","total property area","total land area",
-                        "number of facades","number of bedrooms","terrace area"]
-    df[columns_to_normalize]=preprocessing.minmax_scale(df[columns_to_normalize])
- 
-    #replace True with 1, and False with 0, turn them into numerical data
-    df.replace([True,False],[1,0],inplace=True)
+    # normalize these columns
+    columns_to_normalize = ["latitude", "longitude", "living area", "livable area",
+                            "garden area", "total property area", "total land area",
+                            "number of facades", "number of bedrooms", "terrace area"]
+    df[columns_to_normalize] = preprocessing.minmax_scale(
+        df[columns_to_normalize])
+
+    # replace True with 1, and False with 0, turn them into numerical data
+    df.replace([True, False], [1, 0], inplace=True)
     print("Dataframe has been cleaned.")
     return df
 
-def remove_outliners(df,columns,n_std):
+
+def remove_outliers(df, columns, n_std):
     """Function to remove outliners"""
     for col in columns:
         mean = df[col].mean()
@@ -53,4 +57,3 @@ def remove_outliners(df,columns,n_std):
         df = df[(df[col] <= mean+(n_std*sd))]
         df = df[(df[col] >= mean-(n_std*sd))]
     return df
-
